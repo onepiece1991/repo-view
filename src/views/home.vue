a98d7a1224456d4c4b5660554d93c73a7f86c5bd
onepiece1991 <31947583+onepiece1991@users.noreply.github.com>
2023-03-21 14:32:47 +0800

    gx

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,6 @@
 <template>
 <img alt="Vue logo" src="../assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App333"/>
+  <HelloWorld msg="Welcome to Your Vue.js App444"/>
   <div>home</div>
 </template>
 
