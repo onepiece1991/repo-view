897e19092c043a8dac56893f72b61140917d29b9
onepiece1991 <31947583+onepiece1991@users.noreply.github.com>
2023-03-21 14:39:45 +0800

    更新66

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,6 @@
 <template>
 <img alt="Vue logo" src="../assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App555"/>
+  <HelloWorld msg="Welcome to Your Vue.js App666"/>
   <div>home</div>
 </template>
 
