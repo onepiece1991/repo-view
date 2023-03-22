b741602bed1970591f4e72da96d1e70569dbd429
onepiece1991 <31947583+onepiece1991@users.noreply.github.com>
2023-03-22 14:50:56 +0800

    test03

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,6 @@
 <template>
 <img alt="Vue logo" src="../assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App102"/>
+  <HelloWorld msg="Welcome to Your Vue.js App103"/>
   <div>home</div>
 </template>
 
