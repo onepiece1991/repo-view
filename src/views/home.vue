0ec3cbc2ee7861bff6ce509ac3df855a75099a8b
onepiece1991 <31947583+onepiece1991@users.noreply.github.com>
2023-03-22 14:09:56 +0800

    s101

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,6 @@
 <template>
 <img alt="Vue logo" src="../assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App01"/>
+  <HelloWorld msg="Welcome to Your Vue.js App101"/>
   <div>home</div>
 </template>
 
